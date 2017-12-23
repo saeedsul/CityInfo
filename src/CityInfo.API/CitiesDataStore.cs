0ad718d8155da471c0f46afa20800b07346d4209
saeed <saeed.suleiman@hotmail.co.uk>
2017-12-23 22:00:31 +0000

    1

--- src/CityInfo.API/CitiesDataStore.cs
+++ src/CityInfo.API/CitiesDataStore.cs
@@ -1,13 +1,11 @@
 ﻿using CityInfo.API.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CityInfo.API
 {
     public class CitiesDataStore
     {
+
         public static CitiesDataStore Current { get; } = new CitiesDataStore();
         public List<CityDto> Cities { get; set; }
 
